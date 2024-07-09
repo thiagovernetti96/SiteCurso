04e44e9b202c33beeac3e56eda6efd1caf01f09e
Thiago <thiagovernetti@hotmail.com>
2024-07-09 18:04:14 -0300

    tentativa pre aula

--- resources/js/Carrousel.js
+++ resources/js/Carrousel.js
@@ -1,22 +1,5 @@
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
 });
